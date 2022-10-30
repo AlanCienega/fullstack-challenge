c5ff452c2a7bc1d78173adc43f4be335aff6c1ae
AlanCienega <craulalan115@gmail.com>
2022-10-30 12:09:03 -0600

    lienzo en blanco para dashboard

--- kanban/resources/js/app.js
+++ kanban/resources/js/app.js
@@ -4,26 +4,26 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-import './bootstrap';
-import { createApp } from 'vue';
+import "./bootstrap";
+import { createApp } from "vue";
 
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
  * registering components with the application instance so they are ready
- * to use in your application's views. An example is included for you.
+ * to use in your application's views. An kanban is included for you.
  */
 
 const app = createApp({});
 
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
+import KanbanComponent from "./components/KanbanComponent.vue";
+app.component("kanban-component", KanbanComponent);
 
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * Eg. ./components/KanbanComponent.vue -> <kanban-component></kanban-component>
  */
 
 // Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
@@ -36,4 +36,4 @@ app.component('example-component', ExampleComponent);
  * scaffolding. Otherwise, you will need to add an element yourself.
  */
 
-app.mount('#app');
+app.mount("#app");
